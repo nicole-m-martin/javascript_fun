ee36d3b5ac2c0d6859d892379607037041e728af
Nicole Martin <n.martinpdx@gmail.com>
2022-02-25 19:52:51 -0800

    add loops

---
+++ Loops/loops.js
@@ -0,0 +1,15 @@
+// LOOPS = Repeating Code
+// Multiple types: for loop, while loop, for...of loop, for...in loop
+
+// For Loop Syntax:
+// for(
+//   [initialExpression];
+//   [condition];
+//   [incrementExpression];
+//   )
+
+// 1 initial value
+//
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
